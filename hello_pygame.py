21c5f00acd170cee04e2d929804828189f65e2da
Diego Guimaraes <diegodukao@gmail.com>
2010-10-16 19:36:50 -0300

    changing the animation when the world is close

--- hello_pygame.py
+++ hello_pygame.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 import pygame
+import math
 from animated_sprite import AnimatedSprite, Animation
 from world import World
 
@@ -15,8 +16,9 @@ class Game:
         guy_image = guy_image.convert()
         self.guy = AnimatedSprite([0, 0], guy_image, 3, 4, 10)
         self.guy_idle = self.guy.create_animation([0, 1, 2, 3, 3, 2, 1, 0])
+        self.guy_waving = self.guy.create_animation([7, 8, 9, 10, 11, 11, 10, 9, 8, 7])
         
-        self.guy.set_animation(guy_idle)
+        self.guy.set_animation(self.guy_idle)
         
         world_image = pygame.image.load("data/images/world.png")
         world_image = world_image.convert()
@@ -32,6 +34,13 @@ class Game:
                 elif event.type == pygame.KEYDOWN:
                     self.key_handler(event.key)
             
+            if distance(self.guy, self.world) < 140:
+                if self.guy.animation != self.guy_waving:
+                    self.guy.set_animation(self.guy_waving)
+            else:
+                if self.guy.animation != self.guy_idle:
+                    self.guy.set_animation(self.guy_idle)
+            
             self.guy.update()
             self.screen.blit(self.world.image, self.world.rect)
             self.screen.blit(self.guy.image, self.guy.rect)
@@ -44,6 +53,15 @@ class Game:
         or key == pygame.K_RIGHT):
             self.world.move(key)
             self.screen.fill((0, 0, 0))
+
+def distance(object1, object2):
+    x1 = object1.rect.centerx
+    y1 = object1.rect.centery
+    
+    x2 = object2.rect.centerx
+    y2 = object2.rect.centery
+    
+    return math.sqrt((x1-x2)**2 + (y1-y2)**2)
 
 if __name__ == "__main__":
     game = Game((640,480))
